21d4ad3e16c088c2ccfe643e07f1c4fac0f1400f
sanjay-director <127958856+karan-mango@users.noreply.github.com>
2024-08-16 07:31:13 +0530

    first commit

---
+++ front/src/navbar/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from "@/components/ui/button";
+
+export default function Footer() {
+  return (
+    <div className='bg-black text-white h-[120px] flex flex-col justify-center items-center'>
+      <p className='mb-2'>All rights reserved to SO(IT) and NavCC</p>
+     
+    </div>
+  );
+}
